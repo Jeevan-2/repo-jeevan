be87ad200a91dc6cf3bf85cc3d9fea903b100745
jeevan <rmjeevankiran@gmail.com>
2022-12-01 16:43:26 +0000

    Modifeied file biggestof3.sh

--- biggestof3.sh
+++ biggestof3.sh
@@ -3,10 +3,12 @@
 if [ $1 -gt $2 ] && [ $1 -gt $3 ];
 then
         echo "$1 is bigger than $2 & $3"
+
 elif [ $2 -gt $1 ] && [ $2 -gt $3 ];
 then
         echo "$2 is bigger than $1 & $3"
+
 else
-        echo "$3 is bigger than $1 & $2"
+	echo "$3 is bigger than $1 & $2"
 fi
 
